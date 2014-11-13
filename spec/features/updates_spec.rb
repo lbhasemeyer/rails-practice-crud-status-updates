d7c1b5d95d5e46c9d8c07e49aadd1ecbc952472b
lbhasemeyer <lbhasemeyer@gmail.com>
2014-11-13 10:45:21 -0700

    finished capybara

---
+++ spec/features/updates_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+  feature "updates" do
+    scenario "user creates an update" do
+      visit root_path
+      expect(page).to have_no_content("Kittens! So soft and wonderful!")
+      expect(page).to have_no_content("catlovr1944")
+      expect(page).to have_no_content("1000001")
+      click_on "Create Update"
+      click_on "Create Update"
+      expect(page).to have_content("Status can't be blank")
+      expect(page).to have_content("User can't be blank")
+      expect(page).to have_content("Likes can't be blank")
+      fill_in "Status", with: "Kittens! So soft and wonderful!"
+      fill_in "User", with: "catlovr1944"
+      fill_in "Likes", with: "1000001"
+      click_on "Create Update"
+      expect(page).to have_content("Kittens! So soft and wonderful!")
+      expect(page).to have_content("catlovr1944")
+      expect(page).to have_content("1000001")
+      visit root_path
+      expect(page).to have_content("Kittens! So soft and wonderful!")
+      expect(page).to have_content("catlovr1944")
+      expect(page).to have_content("1000001")
+    end
+  end
